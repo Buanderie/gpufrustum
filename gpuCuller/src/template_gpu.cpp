b6533d0881f8e7760f03051e78bf09c01dd90429
arnaud.vallerent <arnaud.vallerent@bbca828e-1ad7-11de-a108-cd2f117ce590>
2009-04-01 11:33:14 +0000

    - Creation des projets visual pour la lib gpuCuller - Ajout d'un projet de test

---
+++ gpuCuller/src/template_gpu.cpp
@@ -0,0 +1,9 @@
+#include <template_gpu.h>
+
+extern "C"
+void run( int argc, char** argv ); 
+
+void run_Bingo( int argc, char** argv)
+{
+	run( argc, argv ); 
+}
---
+++ test/FirstTest/FirstTest/main.cpp
@@ -0,0 +1,8 @@
+#include <template_gpu.h>
+
+void main( int argc, char** argv); 
+
+void main( int argc, char** argv)
+{
+	run_Bingo( argc, argv );
+}
